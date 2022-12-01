66ea25edcfae72229b8d359ceea8c921819164c3
max221088 <max.work221088@gmail.com>
2022-12-01 17:28:07 +0200

    insert data in tabl

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 document.querySelector('.ring').addEventListener("click", function () {
 	this.classList.toggle('active');
 });
+
+function namePokemon (list){
+    let tableItems = '';
+    list.results.forEach(function (el) {
+        tableItems += "<tr> <td>"+el.name+"</td><td>"+el.url+"</td></tr>";
+    });
+    document.querySelector('tbody').insertAdjacentHTML('beforeend', tableItems)
+};
+
+fetch('https://pokeapi.co/api/v2/pokemon/')
+.then(function (response) {
+   return response.json()
+})
+.then(function (data){
+    namePokemon(data)
+});
