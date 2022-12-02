a6393a73cbc938e9a50177deb5c6b8ba7a4ea63f
max221088 <max.work221088@gmail.com>
2022-12-03 00:06:27 +0200

    add filters

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+const $filt = document.querySelector('.filter');
+let pokemon = [];
+//console.log($filt);
+
 document.querySelector('.ring').addEventListener("click", function () {
 	this.classList.toggle('active');
 });
 
 function namePokemon (list){
     let tableItems = '';
-    list.results.forEach(function (el) {
+    list.forEach(function (el) {
         tableItems += "<tr> <td>"+el.name+"</td><td>"+el.url+"</td></tr>";
     });
-    document.querySelector('tbody').insertAdjacentHTML('beforeend', tableItems)
+    document.querySelector('tbody').insertAdjacentHTML('beforeend', tableItems);
 };
 
 fetch('https://pokeapi.co/api/v2/pokemon/')
@@ -15,5 +19,20 @@ fetch('https://pokeapi.co/api/v2/pokemon/')
    return response.json()
 })
 .then(function (data){
-    namePokemon(data)
+	pokemon = data.results;
+    namePokemon(pokemon);
+	console.log(pokemon);
+});
+
+$filt.addEventListener('input', function (){
+	let query = this.value.toLowerCase();
+	let filtPokemon = pokemon.filter(function(el){
+		if (el.name.toLowerCase().indexOf(query) != -1) {
+			return true;
+		} else {
+			return false;
+		};
+	});
+	console.log(filtPokemon);
+	namePokemon(filtPokemon);
 });
