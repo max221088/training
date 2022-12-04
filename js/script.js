d459af5681a8b63b89d91b5c71a0c9bdb0b3600a
max221088 <max.work221088@gmail.com>
2022-12-04 22:01:30 +0200

    format document

--- js/script.js
+++ js/script.js
@@ -1,53 +1,55 @@
-const $filt = document.querySelector('.filt');
+const $filt = document.querySelector(".filt");
 let pokemon = [];
 //console.log($filt);
-const collapse = document.querySelector('#collapse');
+const collapse = document.querySelector("#collapse");
 //console.log(collapse);
 
-document.querySelector('.ring').addEventListener("click", function () {
-	this.classList.toggle('active');
+document.querySelector(".ring").addEventListener("click", function () {
+	this.classList.toggle("active");
 });
 
-function namePokemon (list){
-	let tableItems = '';
+function namePokemon(list) {
+	let tableItems = "";
 	if (list.length != 0) {
 		list.forEach(function (el) {
-			tableItems += "<tr> <td>"+el.name+"</td><td>"+el.url+"</td></tr>"});
-	} else { 
-		tableItems = "<tr><td>Not Font</td><td>Not Font</td></tr>"
+			tableItems +=
+				"<tr> <td>" + el.name + "</td><td>" + el.url + "</td></tr>";
+		});
+	} else {
+		tableItems = "<tr><td>Not Font</td><td>Not Font</td></tr>";
 	}
-	document.querySelector('tbody').innerHTML = tableItems
-};
+	document.querySelector("tbody").innerHTML = tableItems;
+}
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-.then(function (response) {
-   return response.json()
-})
-.then(function (data){
-	pokemon = data.results;
-    namePokemon(pokemon);
-	//console.log(pokemon);
-});
+fetch("https://pokeapi.co/api/v2/pokemon/")
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		pokemon = data.results;
+		namePokemon(pokemon);
+		//console.log(pokemon);
+	});
 
-$filt.addEventListener('input', function (){
+$filt.addEventListener("input", function () {
 	let query = this.value.toLowerCase();
-	let filtPokemon = pokemon.filter(function(el){
+	let filtPokemon = pokemon.filter(function (el) {
 		if (el.name.toLowerCase().indexOf(query) != -1) {
 			return true;
 		} else {
 			return false;
-		};
-	})
+		}
+	});
 	namePokemon(filtPokemon);
 });
 
-collapse.addEventListener('click' , function(){
-	document.querySelectorAll('.menu-text').forEach(function(el){
-		el.classList.toggle('hidden')
+collapse.addEventListener("click", function () {
+	document.querySelectorAll(".menu-text").forEach(function (el) {
+		el.classList.toggle("hidden");
 	});
-	document.querySelector('.header').classList.toggle('wrap');
-	document.querySelector('.wrapper').classList.toggle('wrap');
-	document.querySelector('.brand').classList.toggle('hidden');
-	document.querySelector('.logo-brand').classList.toggle('active');
-	document.querySelector('.menu').classList.toggle('minimize');
+	document.querySelector(".header").classList.toggle("wrap");
+	document.querySelector(".wrapper").classList.toggle("wrap");
+	document.querySelector(".brand").classList.toggle("hidden");
+	document.querySelector(".logo-brand").classList.toggle("active");
+	document.querySelector(".menu").classList.toggle("minimize");
 });
