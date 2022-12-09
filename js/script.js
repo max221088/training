828a31445214143a6d47785c736c1525339f83df
max221088 <max.work221088@gmail.com>
2022-12-09 22:38:21 +0200

    add modal window to pokemon name

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ let pokemon = [];
 const $collapse = document.querySelector("#collapse");
 const $table = document.querySelector("table");
 const $modal = document.querySelector(".modal-container");
-console.log(document.querySelector(".overlay"));
 
 document.querySelector(".ring").addEventListener("click", function () {
 	this.classList.toggle("active");
@@ -23,7 +22,17 @@ function namePokemon(list) {
 	if (list.length != 0) {
 		list.forEach(function (el) {
 			tableItems +=
-				"<tr> <td>" + el.name + "</td><td>" + el.url + "</td></tr>";
+				"<tr> <td class = pokemon-link" +
+				" " +
+				"data-url=" +
+				el.url +
+				">" +
+				el.name +
+				"</td>" +
+				"<td>" +
+				el.url +
+				"</td>" +
+				"</tr>";
 		});
 	} else {
 		tableItems = "<tr><td>Not Font</td><td>Not Font</td></tr>";
@@ -63,8 +72,42 @@ $collapse.addEventListener("click", function () {
 	document.querySelector(".menu").classList.toggle("minimize");
 });
 
-$table.addEventListener("click", function () {
-	openModal(open);
+$table.addEventListener("click", function (event) {
+	let url = event.target.dataset.url;
+	if (url) {
+		fetch(event.target.dataset.url)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+				let modal =
+					"<h3 class =modal-tittle>" +
+					data.name +
+					"</h3>" +
+					"<div class = front-default>" +
+					"<img src = " +
+					data.sprites.other.dream_world.front_default +
+					">" +
+					"</div>" +
+					"<div class = foot-bar>" +
+					"<img src = " +
+					data.sprites.back_default +
+					">" +
+					"<img src = " +
+					data.sprites.back_shiny +
+					">" +
+					"<img src = " +
+					data.sprites.front_default +
+					">" +
+					"<img src = " +
+					data.sprites.front_shiny +
+					">" +
+					"</div>";
+				document.querySelector(".modal").innerHTML = modal;
+				//console.log(data);
+			});
+		openModal(open);
+	}
 });
 document.querySelector(".overlay").addEventListener("click", function () {
 	openModal(close);
